f6d2b89486c5f67a53c8be55c26c1a9b196dce51
Mark Biggers <mabigger@cisco.com>
2019-03-20 17:03:58 -0400

    updates/docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
-FROM registry.opensource.zalan.do/stups/python:3.6.5-22
+FROM python:3.6-slim-stretch
 
-COPY Pipfile /
-COPY Pipfile.lock /
+# docker build -t connexion-example .
+# docker run -d -p 8080:8080 -v "$PWD":/usr/src/app connexion-example
+# ./test.sh
 
-RUN pipenv install --system --deploy --ignore-pipfile
+ENV PYTHONUNBUFFERED 1
+ENV USER nobody
 
-COPY app.py /
-COPY swagger.yaml /
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
 
-WORKDIR /data
-CMD /app.py
+COPY Pipfile /usr/src/app
+COPY Pipfile.lock /usr/src/app
+
+RUN pip install --no-cache-dir pipenv && \
+    pipenv install --system --deploy --ignore-pipfile
+
+COPY . .
+
+CMD ["python3", "/usr/src/app/app.py"]
--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
-import connexion
 import datetime
-import logging
 
+import logging
+import logzero
+from logzero import logger
+
+import connexion
 from connexion import NoContent
+
+from secrets import (basic_auth, get_secret)
 
 # our memory-only pet storage
 PETS = {}
 
+logzero.loglevel(logging.DEBUG)
+log = logger
+
 
 def get_pets(limit, animal_type=None):
-    return {"pets": [pet for pet in PETS.values() if not animal_type or pet['animal_type'] == animal_type][:limit]}
+    return {"pets":
+            [pet for pet in PETS.values() if not animal_type or
+             pet['animal_type'] == animal_type][:limit]
+            }
 
 
 def get_pet(pet_id):
     pet = PETS.get(pet_id)
-    return pet or ('Not found', 404)
+    log.info("get, pet_id <%s>", pet_id)
+    return pet or ("Not found", 404)
 
 
 def put_pet(pet_id, pet):
     exists = pet_id in PETS
     pet['id'] = pet_id
     if exists:
-        logging.info('Updating pet %s..', pet_id)
+        log.info("update, pet_id <%s>", pet_id)
         PETS[pet_id].update(pet)
     else:
-        logging.info('Creating pet %s..', pet_id)
+        log.info("create, pet_id <%s>", pet_id)
         pet['created'] = datetime.datetime.utcnow()
         PETS[pet_id] = pet
     return NoContent, (200 if exists else 201)
@@ -33,19 +45,21 @@ def put_pet(pet_id, pet):
 
 def delete_pet(pet_id):
     if pet_id in PETS:
-        logging.info('Deleting pet %s..', pet_id)
+        log.info("delete, pet_id <%s>", pet_id)
         del PETS[pet_id]
         return NoContent, 204
     else:
         return NoContent, 404
 
 
-logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__)
-app.add_api('swagger.yaml')
+app.add_api('petstore_swagger.yml')
+
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
+
 application = app.app
+
 
 if __name__ == '__main__':
     # run our standalone gevent server
